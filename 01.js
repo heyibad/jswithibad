bc6ca503a2b8744422e8daaa990019956e42308b
Muhammad Ibad <43316535+theibadofficial@users.noreply.github.com>
2023-08-21 18:55:14 +0000

    done

---
+++ 01.js
@@ -0,0 +1,40 @@
+//Loop in js
+// let i=0;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// for (let ig = 0; ig <=10; ig++) {
+//     const element = ` the no of element is: ${ig}`;
+//     console.log(element);
+    
+// }
+// let v=0;
+// do{ console.log(v);
+//      v++}
+// while(v<=5)
+// do {
+//     console.log(v);
+// } while (condition);
+// function aBac('ib',88){
+     // for (let loo=0;loo<=10;loo++) {
+     //      const x = loo;
+     //      console.log(`${x}, the name of person is ${name} and his marks are: ${num}`)
+     // }
+     
+     
+     // }
+     
+     // const aBac= function(name,num){
+     //      for (let loo=0;loo<=10;loo++) {
+     //           const x = loo;
+     //           console.log(`${x}, the name of person is ${name} and his marks are: ${num}`)
+     //      }
+     // }
+     const aBac=(name,num)=>{
+          for (let loo=0;loo<=10;loo++) {
+               const x = loo;
+               console.log(`${x}, the name of person is ${name} and his marks are: ${num}`)
+          }}
+     aBac('ibad',55)
